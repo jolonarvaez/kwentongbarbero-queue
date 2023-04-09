de0ef757b905ff8fd875c40f36b76def4310c48a
Jolo Narvaez <jose_wilfredo_narvaez@dlsu.edu.ph>
2023-04-09 21:57:22 +0800

    added main page

--- next.config.js
+++ next.config.js
@@ -2,5 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
 }
+css: [
+  '@fortawesome/fontawesome-svg-core/styles.css'
+]
+
+plugins: [
+  '~/plugins/fontawesome.js'
+]
 
 module.exports = nextConfig
