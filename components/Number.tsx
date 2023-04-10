c4f10e3f295283c01eeaf0c923b5b2d1e43d4bac
jolonarvaez <jose_wilfredo_narvaez@dlsu.edu.ph>
2023-04-11 00:36:32 +0800

    added queue funtcionality

---
+++ components/Number.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Number(props: number) {
+    const num = props
+    return (
+        <div>
+            {props}
+        </div>
+    );
+}
+
+export default Number;
