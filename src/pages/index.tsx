ac11dc6ea45ae50e61ef6dbb33047df1e8ef329d
jolonarvaez <jose_wilfredo_narvaez@dlsu.edu.ph>
2023-05-12 16:51:33 +0800

    transferred notice and increased text size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
         alt="logo"
         className="my-5 w-4/6 md:w-1/2 lg:w-2/6"
       />
-      <div className="bg-white w-3/4 lg:w-3/5 h-3/5 rounded-lg text-center align-middle">
+      <div className="bg-white w-3/4 lg:w-3/5 h-2/5 rounded-lg text-center align-middle">
         <div className="flex flex-col h-full justify-center items-center">
           <div className="uppercase text-7xl my-4">Now Serving</div>
           <div className="font-bold text-9xl my-4">
@@ -32,15 +32,15 @@ export default function Home() {
               useGrouping: false,
             })}
           </div>
-          <div className="mx-4">
-            <p>
-              <span className="uppercase font-bold">Notice:</span> Please note
-              that if you are not present when your number is called,<br></br>
-              the next number shall be accommodated. You will then be given a
-              new number. <br></br>Thank you for your understanding.
-            </p>
-          </div>
         </div>
+      </div>
+      <div className="m-4 text-white text-4xl text-center">
+        <p>
+          <span className="uppercase font-bold">Notice:</span> Please note that
+          if you are not present when your number is called,<br></br>
+          the next number shall be accommodated. You will then be given a new
+          number. <br></br>Thank you for your understanding.
+        </p>
       </div>
       <div className="my-5 grid grid-rows-1 grid-flow-col gap-6 text-xl">
         <button
